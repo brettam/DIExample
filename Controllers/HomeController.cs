06884be4a186520d42efc32ce6d56c81fb62791a
Brett AM <brett.morrell@hotmail.com>
2023-08-29 11:40:10 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using ServiceContracts;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // should never create an object of a service class in controller
+        // 
+        // instead should use dependency injection
+        private readonly ICitiesService _citiesService;
+
+        public HomeController(ICitiesService citiesService)
+        {
+            _citiesService = citiesService;//new CitiesService();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Services;
+using ServiceContracts;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.Add(new ServiceDescriptor(
+    typeof(ICitiesService),
+    typeof(CitiesService),
+    ServiceLifetime.Transient
+        ));
+
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
